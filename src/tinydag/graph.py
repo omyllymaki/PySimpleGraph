0175e2e9cac0d1c0bbeceefd1ab8ca57eb10babd
Ossi Myllymäki <omyllymaki@gmail.com>
2024-05-06 17:57:24 +0300

    Refactor parallel processing

--- src/tinydag/graph.py
+++ src/tinydag/graph.py
@@ -3,8 +3,9 @@ import multiprocessing
 import os
 import time
 from copy import deepcopy
+from multiprocessing import Queue, Process
 from os.path import join
-from typing import List, Union, Optional
+from typing import List, Union, Optional, Dict
 
 from tinydag.exceptions import InvalidGraphError, MissingInputError
 from tinydag.node import Node
@@ -237,70 +238,78 @@ class Graph:
         return inputs
 
     def _run_nodes_parallel(self, input_data: Optional[dict]) -> dict:
+        # Init variables shared between the processes
         manager = multiprocessing.Manager()
-        inputs = manager.dict(input_data) if input_data is not None else manager.dict()
+        inputs = manager.dict(input_data) \
+            if input_data is not None \
+            else manager.dict()
         exception_queue = multiprocessing.Queue()
-        results_queue = multiprocessing.Queue()
-
-        def node_task(node_index):
-            node = self._nodes[node_index]
-            logger.debug(f"Launched task for node {node}, process id {multiprocessing.current_process().pid}")
-            try:
-                results = self._run_node_and_cache(node, node_input_data)
-                if results is not None:
-                    inputs.update(results)
-                results_queue.put(node_index)
-                return
-            except Exception as e:
-                logger.warning(f"Node {node} raised exception {e}")
-                exception_queue.put(e)
-                results_queue.put(None)
-                return
 
         # Loop until all the nodes are executed
         nodes_to_execute = [i for i in range(len(self._nodes))]
-        node_indices_in_processing, processes_running = [], []
+        running_processes = {}
         while len(nodes_to_execute) > 0:
-
             # Start process for every node that has all the inputs available and is not already running
-            node_indices_to_check = set(nodes_to_execute) - set(node_indices_in_processing)
-            for node_index in node_indices_to_check:
-                node = self._nodes[node_index]
-                node_input_data = self._collect_node_input_data(node, inputs)
-                if len(node_input_data) < len(node.inputs):
-                    continue
-
-                process = multiprocessing.Process(target=node_task, args=(node_index,))
-                process.start()
-                processes_running.append(process)
-                node_indices_in_processing.append(node_index)
+            running_processes = self._start_node_processes(inputs, nodes_to_execute, running_processes, exception_queue)
 
             # Wait until at least one of the running processes has finished
-            process_finished_flag = False
-            while not process_finished_flag:
-                for process in processes_running:
-                    if not process.is_alive():
-
-                        logger.debug(f"Process {process.pid} has finished")
-
-                        while not exception_queue.empty():
-                            exception = exception_queue.get()
-                            logger.warning(f"Received exception {exception}")
-                            raise exception
-
-                        while not results_queue.empty():
-                            node_index = results_queue.get()
-                            nodes_to_execute.remove(node_index)
-                            node_indices_in_processing.remove(node_index)
-                            logger.info(
-                                f"Number of nodes: \n " +
-                                f"in processing: {len(node_indices_in_processing)} \n " +
-                                f"unfinished: {len(nodes_to_execute)} \n " +
-                                f"finished: {len(self._nodes) - len(nodes_to_execute)}")
-
-                        process_finished_flag = True
+            node_index_finished = self._wait_process_to_finish(running_processes, exception_queue)
+            nodes_to_execute.remove(node_index_finished)
+            running_processes.pop(node_index_finished)
+            logger.info(
+                f"Number of nodes: \n " +
+                f"in processing: {len(running_processes)} \n " +
+                f"unfinished: {len(nodes_to_execute)} \n " +
+                f"finished: {len(self._nodes) - len(nodes_to_execute)}")
 
         return inputs
+
+    def _node_task(self,
+                   node_index: int,
+                   node_input_data: list,
+                   inputs: dict,
+                   exception_queue: Queue) -> None:
+        node = self._nodes[node_index]
+        logger.debug(f"Launched task for node {node}, process id {multiprocessing.current_process().pid}")
+        try:
+            results = self._run_node_and_cache(node, node_input_data)
+            if results is not None:
+                inputs.update(results)  # Lock is built-in, no need to do it manually
+            return
+        except Exception as e:
+            logger.warning(f"Node {node} raised exception {e}")
+            exception_queue.put(e)  # Lock is built-in, no need to do it manually
+            return
+
+    @staticmethod
+    def _wait_process_to_finish(running_processes: Dict[int, Process], exception_queue: Queue) -> int:
+        while True:
+            for node_index, process in running_processes.items():
+                if not process.is_alive():
+                    logger.debug(f"Process {process.pid} has finished")
+                    while not exception_queue.empty():
+                        exception = exception_queue.get()
+                        logger.warning(f"Received exception {exception}")
+                        raise exception
+
+                    return node_index
+
+    def _start_node_processes(self,
+                              inputs: dict,
+                              nodes_to_execute: List[int],
+                              running_processes: Dict[int, Process],
+                              exception_queue: Queue) -> Dict[int, Process]:
+        node_indices_to_check = set(nodes_to_execute) - set(running_processes.keys())
+        for node_index in node_indices_to_check:
+            node = self._nodes[node_index]
+            node_input_data = self._collect_node_input_data(node, inputs)
+            if len(node_input_data) < len(node.inputs):
+                continue
+            args = (node_index, node_input_data, inputs, exception_queue)
+            process = multiprocessing.Process(target=self._node_task, args=args)
+            process.start()
+            running_processes[node_index] = process
+        return running_processes
 
     def _create_output(self, inputs: dict) -> dict:
         results = {}
